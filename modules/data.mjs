9c5a009d92d449dff7083bc163e403eebcd62d57
Criss Alvianto <criss.alvianto@hotmail.com>
2020-10-13 00:59:41 +0200

    login funktionalitet och json fil
    La till event handler till logga in knappen. Knappen hänvisar nu till Kanban vid lyckad inloggning. Skapade alla användare i en JSON fil. Skapade login authentication funktion.

--- modules/data.mjs
+++ modules/data.mjs
@@ -1,24 +1,33 @@
 import { page } from './page.mjs';
 import { eventHandlers } from "./eventHandlers.mjs";
+
 export const data = {
-    loginAuthentication: function()
-    {
-        let test = "test";
+    getData: async function(jsonName) {
+        try {
+            let response = await fetch(jsonName);
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    },
 
-        let userNameInp = document.getElementById("username").value;
-        let passwordInp = document.getElementById("password").value;
-        document.getElementById("wrapper").innerHTML = page.getBoardPage();
+    
+    loginAuthentication: async function() {
+        let arrayUserObject = await data.getData('users.json');
+        let userNameInput = document.getElementById("username").value;
+        let passwordInput = document.getElementById("password").value;
 
-        if(userNameInp == test && passwordInp == test){
+        //Kollar ifall det finns en matchande användare
+        let matchedUser = arrayUserObject.some(user => user.username == userNameInput && user.password == passwordInput);
+
+        if(matchedUser){
             document.getElementById("wrapper").innerHTML = page.getBoardPage();
-            eventHandlers.addOnAddCardBtnClickEventHandlers();
+            eventHandlers.addOnAddCardBtnClickEventHandlers(); //Lägger till event handlers på alla "lägg till nytt kort"-knappar
         }
 
-        // 
+        // Måste meddela användaren att inloggningen misslyckades.
         else{
             console.log("Fel lösen/användarnamn!");
         }
-
-
     }
 }
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 import { page } from './modules/page.mjs';
 import { data } from './modules/data.mjs';
 import { eventHandlers } from './modules/eventHandlers.mjs';
-
 document.body.onload = script;
 
 function script() {
     document.getElementById("wrapper").innerHTML = page.getFrontPage();
+    eventHandlers.addOnLoginBtnClickEventHandler();                             //Lägger till event handler till "Logga in"-knappen.
     //document.getElementById("wrapper").innerHTML = page.getBoardPage();
     //eventHandlers.addOnAddCardBtnClickEventHandlers();                          //Lägger till event handlers på alla "lägg till nytt kort"-knappar.
-    eventHandlers.addOnLoginBtnClickEventHandler();
 }
