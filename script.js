2142e62f08db44ba2b63fe7368cdb64b8d3f4116
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-10-12 11:22:25 +0200

    Add cards *work in progress
    Lagt till event-lyssnare för alla kolumnet för
    lägg-till-nytt-kort-knappar
    
    *Work in progress

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ document.body.onload = script;
 function script(){
     //document.getElementById("wrapper").innerHTML = page.getFrontPage();
     document.getElementById("wrapper").innerHTML = page.getBoardPage();
-    
+    addOnAddCardClickEventHandlers();
 
 }
 
 
+
+
+////work in progress add cards anthon
+function addOnAddCardClickEventHandlers() {
+    console.log("add card");
+    const addCardBtns = document.getElementsByClassName("add-card-button");
+    Object.keys(addCardBtns).forEach(key=>{
+        addCardBtns[key].addEventListener("click", e=>onAddCardClickEventHandler(e))
+        console.log(addCardBtns[key]);
+    });
+}
+
+function onAddCardClickEventHandler(e){
+    alert("click",e.target);
+}
